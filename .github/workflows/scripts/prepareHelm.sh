c43309c4e00f18c2fc960ac5b9c7a991105e9d5d
Ryan Faircloth <ryan@dss-i.com>
2022-07-01 11:30:45 -0400

    fix: script

--- .github/workflows/scripts/prepareHelm.sh
+++ .github/workflows/scripts/prepareHelm.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 yq ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" charts/s3proxy/Chart.yaml
 helm package charts/s3proxy
--- .github/workflows/scripts/publishHelm.sh
+++ .github/workflows/scripts/publishHelm.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+set -e
 helm push $(ls s3proxy-*.tgz) oci://ghcr.io/$GITHUB_REPOSITORY/charts
