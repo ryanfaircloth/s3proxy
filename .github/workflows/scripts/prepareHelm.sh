66ac6db67f7630a536e811dbbcdb7c5432ef5d1f
Ryan Faircloth <ryan@dss-i.com>
2022-07-06 08:40:51 -0400

    fix: publish chart

--- .github/workflows/scripts/prepareHelm.sh
+++ .github/workflows/scripts/prepareHelm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #
-yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" charts/s3proxy/Chart.yaml
-helm package charts/s3proxy
+yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" chart/Chart.yaml
+helm package chart
